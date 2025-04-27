ef462facb58d96be02fb0e2898c98edf43fdd997
Md. Asif Khan <20240659130asif@juniv.edu>
2025-04-27 22:36:12 +0600

    Create Task-01.c
    Finding maximum of each row of a 2D array without using array

---
+++ Lab-01/Task-01.c
@@ -0,0 +1,24 @@
+/*Write a C program to input two integers R, C,
+representing the rows and columns of a 2D array. Next,
+input R lines. Each line will have C integers. Finally, print
+the maximum number of each row.*/
+
+#include <stdio.h>
+
+int main() {
+    int R,C;
+    scanf("%i%i",&R,&C);
+    
+    int max = 0,a;
+    while(R--){
+        for(int i=0;i<C;i++){
+        scanf("%i",&a);
+        if(a>max) 
+        max =a;
+    }
+    printf("%i",max);
+
+    }
+    
+    return 0;
+}
