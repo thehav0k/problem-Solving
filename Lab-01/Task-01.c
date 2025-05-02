858496ebae5f7e1bfbe8614632bc33451a73d830
Md. Asif Khan <20240659130asif@juniv.edu>
2025-05-03 01:28:39 +0600

    Update Task-01.c

--- Lab-01/Task-01.c
+++ Lab-01/Task-01.c
@@ -4,13 +4,14 @@ input R lines. Each line will have C integers. Finally, print
 the maximum number of each row.*/
 
 #include <stdio.h>
+#include<climits>
 
 int main() {
     int R,C;
     scanf("%i%i",&R,&C);
     
-    int max = 0,a;
     while(R--){
+        int max=INT_MIN,a;
         for(int i=0;i<C;i++){
         scanf("%i",&a);
         if(a>max) 
@@ -19,6 +20,6 @@ int main() {
     printf("%i \n",max);
 
     }
-    
+
     return 0;
 }
